480e5cc470be65d61f5772afcd76a89b13934bd7
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-02 11:12:32 -0700

    Reverse string using ES2015 for loop

--- javascript/puzzles/reversestring/index.js
+++ javascript/puzzles/reversestring/index.js
@@ -5,8 +5,19 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
-
 function reverse(str) {
+  let reversed = "";
+
+  // ES2015 syntax for for loop
+  for (let character of str) {
+    // add the current character to the start of the string reversed
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+function reverse2(str) {
   // take the string and turn it into an array with every element in that array consisting of a
   // different character from the string
   const arr = str.split("");
