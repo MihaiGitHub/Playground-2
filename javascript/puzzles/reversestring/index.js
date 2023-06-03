0ea9bca456e9df63dbfac00f498357028e7e02c9
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-03 11:05:33 -0700

    Reverse string third method

--- javascript/puzzles/reversestring/index.js
+++ javascript/puzzles/reversestring/index.js
@@ -5,7 +5,16 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+
 function reverse(str) {
+  // turn string to array and use reduce to condense all values in the array to a single string value
+  return str.split("").reduce((reversed, character) => {
+    // arg1 - arrow function, arg2 - starting an initial value for the function
+    return character + reversed;
+  }, "");
+}
+
+function reverse2(str) {
   let reversed = "";
 
   // ES2015 syntax for for loop
@@ -17,7 +26,7 @@ function reverse(str) {
   return reversed;
 }
 
-function reverse2(str) {
+function reverse3(str) {
   // take the string and turn it into an array with every element in that array consisting of a
   // different character from the string
   const arr = str.split("");
