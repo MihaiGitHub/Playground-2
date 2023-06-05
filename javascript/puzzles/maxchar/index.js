b2c52d45beb2a3c577692e02dd5faae330b4205d
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-05 14:14:15 -0700

    Most common character in string solution

---
+++ javascript/puzzles/maxchar/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  // use a character map
+  const charMap = {};
+
+  let max = 0;
+  let maxChar = "";
+
+  // itirate through string and build out character map
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  // itirate through the object and find max number and max character
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+module.exports = maxChar;
