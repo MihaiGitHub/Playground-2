832b707630cace263ad760df9ce7e4da3dfea3b7
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-11 14:43:55 -0700

    Second solution to Capitalize

--- javascript/puzzles/capitalize/index.js
+++ javascript/puzzles/capitalize/index.js
@@ -21,4 +21,18 @@ function capitalize(str) {
   return words.join(" "); // adds space between each array element when joining into a string
 }
 
+function capitalize2(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
 module.exports = capitalize;
