ac1bedb58f732352215070395fd0b70def2494d6
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-10 17:37:13 -0700

    Capitalize string solution

---
+++ javascript/puzzles/capitalize/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  // create an array to hold the separate words of the string
+  const words = [];
+
+  // split string by the spaces (to get separate words)
+  for (let word of str.split(" ")) {
+    // capitalize first letter, concatenate to the rest of the string and push it into words array
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  // turn words array into a string and return it
+  return words.join(" "); // adds space between each array element when joining into a string
+}
+
+module.exports = capitalize;
