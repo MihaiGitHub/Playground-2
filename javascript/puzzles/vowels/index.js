10c239694296f531e39ea1f4f7fbe618cb9bc1ff
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-18 17:20:23 -0700

    Vowels solution 2

--- javascript/puzzles/vowels/index.js
+++ javascript/puzzles/vowels/index.js
@@ -8,6 +8,14 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
+  // g = don't stop at first match, find them all, i = case insensitive
+  // match returns array with matches; if no matches found it will return null
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0;
+}
+
+function vowels2(str) {
   let count = 0;
   const checker = "aeiou";
 
