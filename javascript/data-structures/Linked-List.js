11743ad8c60da4dd5eaa01b02cdd8934719d7a02
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-27 14:55:25 -0700

    Linked List data structure

---
+++ javascript/data-structures/Linked-List.js
@@ -0,0 +1,12 @@
+// Nodes linked together
+// Every list has a head node and a tail node
+const nodeOne = {
+  data: 123,
+};
+
+const nodeTwo = {
+  data: 456,
+};
+
+// form reference between the two nodes
+nodeOne.next = nodeTwo;
