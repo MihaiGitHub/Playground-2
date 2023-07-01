982c01d025027d10eead05bf059f999cecca8bf5
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-01 12:06:03 -0700

    Remove method for Node class

--- javascript/data-structures/Tree.js
+++ javascript/data-structures/Tree.js
@@ -7,4 +7,11 @@ class Node {
   add(data) {
     this.children.push(new Node(data));
   }
+
+  remove(data) {
+    this.children = this.children.filter((node) => {
+      // keep in array copy every everything that is not equal to argument
+      return node.data !== data;
+    });
+  }
 }
