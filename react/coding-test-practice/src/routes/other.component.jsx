a6469ed10625a75daa8875ef52d10000e346ccd8
mihai <mihai.sanfran@gmail.com>
2025-05-07 13:23:39 -0700

    Target component lifecycles

--- react/coding-test-practice/src/routes/other.component.jsx
+++ react/coding-test-practice/src/routes/other.component.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Other = () => {
-  return <div>Other</div>;
+  const [name, setName] = useState("Benson");
+
+  useEffect(() => {
+    console.log("component mounted");
+
+    return () => {
+      console.log("component unmounted");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("component updated");
+  }, [name]);
+
+  const handleClick = () => {
+    setName("Jon");
+  };
+
+  return (
+    <div>
+      <h1>Other</h1>
+      <div>
+        Details: {name}
+        <button onClick={handleClick}>Update State</button>
+      </div>
+    </div>
+  );
 };
 
 export default Other;
